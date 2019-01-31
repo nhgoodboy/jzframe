b4150a85445b40ee9d39fe1ccc524203233ab905
nhgoodboy <evan.yjz@gmail.com>
2019-01-31 17:37:29 +0800

    yjz 退出登录清除session

--- src/main/java/com/musikouyi/jzframe/controller/LoginController.java
+++ src/main/java/com/musikouyi/jzframe/controller/LoginController.java
@@ -10,8 +10,10 @@ import com.musikouyi.jzframe.utils.JwtTokenUtil;
 import com.musikouyi.jzframe.utils.ResultUtil;
 import com.musikouyi.jzframe.utils.SpringContextHolder;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,7 +37,7 @@ public class LoginController {
 
         UsernamePasswordToken token = new UsernamePasswordToken(username, password);
         token.setRememberMe(true);
-
+        ensureUserIsLoggedOut();
         try {
             subject.login(token);
         } catch (AuthenticationException e) {
@@ -49,8 +51,31 @@ public class LoginController {
         return ResultUtil.success(result);
     }
 
+    // Logout the user fully before continuing.(解决sessionId过期的问题)
+    private void ensureUserIsLoggedOut() {
+        try {
+            // Get the user if one is logged in.
+            Subject currentUser = SecurityUtils.getSubject();
+            if (currentUser == null){
+                return;
+            }
+            // Log the user out and kill their session if possible.
+            currentUser.logout();
+            Session session = currentUser.getSession(false);
+            if (session == null){
+                return;
+            }
+            session.stop();
+        } catch (Exception e) {
+            // Ignore all errors, as we're trying to silently
+            // log the user out.
+            log.info(String.valueOf(e));
+        }
+    }
+
     @GetMapping(ControllerMapping.DO_LOGOUT)
     public Result doLogout() {
+        ensureUserIsLoggedOut();
         return ResultUtil.success();
     }
 }
